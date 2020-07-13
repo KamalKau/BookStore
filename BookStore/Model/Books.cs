dd1d2a7d51d951e6532831d158ce147633e21250
Kamal <kkaur337@my.centennialcollege.ca>
2020-07-13 02:15:07 -0400

    Add project files.

---
+++ BookStore/Model/Books.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Model
+{
+    public class Books
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+    }
+}
